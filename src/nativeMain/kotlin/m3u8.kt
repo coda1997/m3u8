6a653c9cb399c3fd76a0439b2cbafe804624a370
DadaChen1997 <overlooker1997@gmail.com>
2020-10-02 18:01:18 +0800

    not done yet.

--- src/nativeMain/kotlin/m3u8.kt
+++ src/nativeMain/kotlin/m3u8.kt
@@ -1,3 +1,16 @@
+import io.ktor.client.*
+import io.ktor.client.request.*
 
-fun m3u8Download(url: String){
+suspend fun m3u8Download(url: String){
+    val client = HttpClient()
+    val m3u8Header = client.get<String> {
+        url(url)
+    }
+
+    //TODO
+    //1st, to parser the file of m3u8, getting file path of each slides
+    //2nd, using multi-thread to fetch all videos and con them.
+    //3rd, write into a ts file.
+    //done
+
 }
--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,5 +1,12 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 fun main() {
-    println("Hello, Kotlin/Native!")
+    GlobalScope.apply {
+        launch {
+            m3u8Download("http://sample.m3u8")
+        }
+    }
 }
 
 
