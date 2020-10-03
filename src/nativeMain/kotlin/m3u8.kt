d5beecf44834842e7f381753000f169e2a6fbc17
DadaChen1997 <overlooker1997@gmail.com>
2020-10-03 10:32:05 +0800

    not done yet.

--- src/nativeMain/kotlin/m3u8.kt
+++ src/nativeMain/kotlin/m3u8.kt
@@ -1,30 +1,43 @@
 import io.ktor.client.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
-suspend fun m3u8Download(url: String, outputPath:String){
+fun m3u8Download(url: String, outputPath: String) = GlobalScope.launch {
     val client = HttpClient()
     val m3u8Header = client.get<String> {
         url(url)
     }
-    if (!formatCheck(m3u8Header)){
+    if (!formatCheck(m3u8Header)) {
         println("The content from ${url}, is not m3u8 !")
-        return
+        return@launch
     }
 
-    //TODO
     //1st, to parser the file of m3u8, getting file path of each slides
+    m3u8Header.split('\n').filter { !it.startsWith('#') }.map {
+        val res = async {
+            client.get<ByteArray> {
+                url(it)
+            }
+        }
+        return@map res
+    }.forEach { slice->
+        //TODO
+    }
+
     //2nd, using multi-thread to fetch all videos and con them.
     //3rd, write into a ts file.
     //done
-
 }
 
-fun formatCheck(value:String):Boolean{
+
+fun formatCheck(value: String): Boolean {
     val contents = value.split('\n')
-    if(contents.isEmpty()){
+    if (contents.isEmpty()) {
         return false
     }
-    if(!contents[0].contains("#EXTM3U")) {
+    if (!contents[0].contains("#EXTM3U")) {
         return false
     }
     //further format checker place here.
--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -2,11 +2,8 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 fun main() {
-    GlobalScope.apply {
-        launch {
-            m3u8Download("http://sample.m3u8","sample path")
-        }
-    }
+    m3u8Download("http://sample.m3u8", "sample path")
+
 }
 
 
